4a33bbdf1baa907ac4738078d1dc71b48ea661e7
Roger Cendrós <rogercbe@gmail.com>
2016-02-26 17:51:32 +0100

    homepage

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Favors4Good</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/css/global.css">
+</head>
+<body>
+	<div class="container">
+		
+	</div>
+</body>
+</html>
