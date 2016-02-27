54e72e3d09c671d28a0b448d95c11393fe86bff3
Roger Cendrós <rogercbe@gmail.com>
2016-02-27 10:47:40 +0100

    lat/long

--- resources/views/pages/list.blade.php
+++ resources/views/pages/list.blade.php
@@ -15,7 +15,10 @@
         @foreach($favors as $favor)
         <div class="item col-xs-4 col-lg-4">
             <div class="thumbnail">
-                <img class="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                <iframe width="355" height="177" frameborder="0" marginheight="0" marginwidth="0"
+                        src="https://maps.google.com/maps?q={{$favor->lat}},{{$favor->long}}&hl=es;z=14&amp&output=embed">
+                </iframe>
+                <!-- <img class="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" /> -->
                 <div class="caption">
                     <h4 class="group inner list-group-item-heading">
                         {{$favor->titol}}</h4>
