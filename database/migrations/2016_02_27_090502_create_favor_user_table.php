09eefc5cc6034a2e3ee822646b97c1628ee366c7
Roger Cendrós <rogercbe@gmail.com>
2016-02-27 10:10:52 +0100

    migration

---
+++ database/migrations/2016_02_27_090502_create_favor_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFavorUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favor_users', function (Blueprint $table) {
+            $table->integer('favor_id')->unsiged();
+            $table->integer('user_id_rebut')->unsiged();
+            $table->integer('user_id_donant')->unsiged();
+            $table->boolean('fet');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('favor_users');
+    }
+}
