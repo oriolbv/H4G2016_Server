cb88f694ea02024a650b81fedd2ffebf099f4882
Roger Cendrós <rogercbe@gmail.com>
2016-02-27 10:53:40 +0100

    masonry grid

--- resources/views/pages/list.blade.php
+++ resources/views/pages/list.blade.php
@@ -39,5 +39,13 @@
         @endforeach
     </div>
 </div>
-
+<script>
+    $('.products').masonry({
+        // set itemSelector so .grid-sizer is not used in layout
+        itemSelector: '.item',
+        // use element for option
+        // columnWidth: '.grid-sizer',
+        // percentPosition: true
+    })
+</script>
 @stop
