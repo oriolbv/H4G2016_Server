2af745a8921850dad39fbe226696f647bc1237a3
Roger Cendrós <rogercbe@gmail.com>
2016-02-27 13:07:59 +0100

    add fet fillable

--- app/FavorUser.php
+++ app/FavorUser.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class FavorUser extends Model
 {
-    protected $fillable = ['favor_id', 'user_id_rebut', 'user_id_donant'];
+    protected $fillable = ['favor_id', 'user_id_rebut', 'user_id_donant', 'fet'];
 }
